e9f3c699ed65a6c08dcb620080158888286496ce
Wisnu Pramoedya <wisnupramoedya@gmail.com>
2023-01-28 14:29:04 +0700

    fix: update material

---
+++ src/konsep-class.ts
@@ -0,0 +1,11 @@
+// Konsep Class di Typescript
+// - Class
+// - Access Modifiers
+//   - Private
+//   - Public
+//   - Protected
+//   - Readonly Properties
+// - Inheritance
+//   - Constructor
+//   - Method Overriding
+// - Static
